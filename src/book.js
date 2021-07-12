7e7472f89ca924dc915752c5c6b7efbd2fc2f652
Min Park <minsungpark.msp@gmail.com>
2021-07-11 20:24:02 -0600

    Min Park Project 1 work

--- src/book.js
+++ src/book.js
@@ -1,12 +1,56 @@
-function createTitle() {
-  
+//*********** createTitle CODE **********************
+function createTitle(bookCloud) {
+  return ('The ' + bookCloud);
+};
+
+//*********** buildMainCharacter CODE ***************
+function buildMainCharacter(name, age, pronouns) {
+  //create character object
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+//***************** saveReview CODE *********************
+function saveReview(review, reviews) {
+	// Loop through reviews, check if already exists
+	for(var index = 0; index < reviews.length; index++) {
+		if(reviews[index] === review) {
+			return;
+		}
+	}
+  // If we make it here, we can add
+	reviews.push(review);
+}
+
+//************ calculatePageCount CODE **************
+function calculatePageCount(title) {
+  return title.length * 20;
+}
+
+//************** writeBook CODE *********************
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: bookGenre
+  }
+}
+//************ editBook ********************
+function editBook(book) {
+  book.pageCount = calculatePageCount(book.title) * 0.75;
+  return;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,34 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+  //add book to shelf
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(bookTitle, shelf) {
+  for (var index = 0; index < shelf.length; index++) {
+    if (shelf[index].title === bookTitle) {
+      shelf.splice(index, 1);
+    }
+  }
+  return shelf;
+}
+
+function listTitles(shelf) {
+  var titles = [0,1,2];
+  // Think we would do a conditional inside of a loop here?  IM OUT OF TIME!!!!!
+}
+
+
+
+
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
